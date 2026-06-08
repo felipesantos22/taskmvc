ca05c30bc16b3358321bd2f656eb3186e0da355a
felipesantos22 <felipevs2018@gmail.com>
2026-06-08 17:11:48 -0300

    change css

---
+++ taskmvc/Controllers/ContactController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace taskmvc.Controllers;
+
+public class ContactController : Controller
+{
+    public ActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ taskmvc/Controllers/PolicyController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace taskmvc.Controllers
+{
+    public class PolicyController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
